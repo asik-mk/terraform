ca9d7aaa3d1ae08ca5522f9124083d801d7e182d
A   Y    A   S <alluru.yasmine@gmail.com>
2024-01-11 20:16:07 +0530

    hi

--- ec2/variables.tf
+++ ec2/variables.tf
@@ -8,3 +8,28 @@ variable "instance_type" {
   default = "t2.micro"
 
 }
+
+variable "sg-name" {
+    type = string
+  default = "allow_all"
+}
+
+variable "sg-description" {
+    type = string
+  default = "allow_all"
+}
+
+variable "inbound_from_port" {
+    #type = string
+  default = 0
+}
+
+variable "inbound_to_port" {
+    #type = string
+  default = 0
+}
+
+variable "cidr_blocks" {
+    #type = string
+  default = ["0.0.0.0/0"]
+}
